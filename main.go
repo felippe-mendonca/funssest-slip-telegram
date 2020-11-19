7202419909c929f367525078215475d858439cdd
Felippe Mendonça <mendonca.felippe@gmail.com>
2020-11-18 22:28:46 -0300

    feat: changes to webhook flow

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"os"
 	"strconv"
 
@@ -10,19 +12,33 @@ import (
 )
 
 func main() {
-	accessToken := os.Getenv("TELEGRAM_ACCESS_TOKEN")
+	telegramAccessToken := os.Getenv("TELEGRAM_ACCESS_TOKEN")
+	telegramWebhookURL := os.Getenv("TELEGRAM_WEBHOOK_URL")
 	telegramDebug, _ := strconv.ParseBool(os.Getenv("TELEGRAM_DEBUG"))
+	httpListenServer := os.Getenv("HTTP_LISTEN_SERVER")
 
-	bot, err := tgbotapi.NewBotAPI(accessToken)
+	bot, err := tgbotapi.NewBotAPI(telegramAccessToken)
 	if err != nil {
 		panic(err)
 	}
-
 	bot.Debug = telegramDebug
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
 
-	updates, err := bot.GetUpdatesChan(u)
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	_, err = bot.SetWebhook(tgbotapi.NewWebhook(telegramWebhookURL + "/" + bot.Token))
+	if err != nil {
+		log.Fatal(err)
+	}
+	info, err := bot.GetWebhookInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if info.LastErrorDate != 0 {
+		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
+	}
+	updates := bot.ListenForWebhook("/" + bot.Token)
+	go http.ListenAndServe(httpListenServer, nil)
+
 	for update := range updates {
 		tgbot.ProcessUpdate(bot, update)
 	}
